ca4b7cffac6aff1d7994c7c930f1d73457cf70fe
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2024-12-11 17:08:15 +0100

    Add: solution to part 2 of day-11 (speed up part 1)

--- day-11/puzzle.go
+++ day-11/puzzle.go
@@ -5,24 +5,19 @@ import (
 	"log"
 	"math"
 	"os"
-	"runtime"
 	"strconv"
 	"strings"
-	"sync"
 )
 
 // arang - current arangement of stones
-func AfterBlink(arang []int) []int {
-	newArang := make([]int, 0, len(arang))
-
-	for _, stone := range arang {
+func AfterBlink(freq map[int]int) (newFreq map[int]int, total int) {
+	newFreq = make(map[int]int)
+	total = 0
+	for stone, occurences := range freq {
+		total += occurences // every stone is at least replaced by one
 		if stone == 0 {
-			newArang = append(newArang, 1)
-			continue
-		}
-
-		stoneDigits := int(math.Log10(float64(stone))) + 1
-		if stoneDigits%2 == 0 {
+			newFreq[1] += occurences
+		} else if stoneDigits := int(math.Log10(float64(stone))) + 1; stoneDigits%2 == 0 {
 			stoneDigitsS := strconv.Itoa(stone)
 			leftStoneS := stoneDigitsS[:stoneDigits/2]
 			rightStoneS := stoneDigitsS[stoneDigits/2:]
@@ -31,25 +26,27 @@ func AfterBlink(arang []int) []int {
 			if err != nil {
 				panic(err)
 			}
+			newFreq[leftStone] += occurences
 
 			rightStone, err := strconv.Atoi(rightStoneS)
 			if err != nil {
 				panic(err)
 			}
-			newArang = append(newArang, leftStone, rightStone)
-			continue
-		}
+			newFreq[rightStone] += occurences
 
-		newArang = append(newArang, stone*2024)
+			total += occurences // as there are two numbers added
+		} else {
+			newFreq[stone*2024] += occurences
+		}
 	}
-	return newArang
+
+	return newFreq, total
 }
 
 func main() {
 	iterations := flag.Int("iterations", 25, "Number of iterations to run")
 	verbose := flag.Bool("verbose", false, "Output stones after each iteration")
 	filename := flag.String("input", "", "Input file with initial arrangement of stones")
-	cores := flag.Int("cores", runtime.NumCPU(), "Number of cores to use")
 	flag.Parse()
 
 	inputB, err := os.ReadFile(*filename)
@@ -58,58 +55,26 @@ func main() {
 	}
 	input := strings.Trim(string(inputB), "\n")
 	arangStr := strings.Split(input, " ")
-	arang := make([]int, len(arangStr))
 
-	for i, str := range arangStr {
-		arang[i], err = strconv.Atoi(str)
+	// Create initial frequencies
+	freq := make(map[int]int)
+	for _, str := range arangStr {
+		stone, err := strconv.Atoi(str)
 		if err != nil {
 			panic(err)
 		}
-	}
-	log.Printf("Core count used: %d\n", *cores)
-	log.Printf("Initial Arrangement:\n%v\n", arang)
+		freq[stone]++
 
+	}
+	log.Printf("Initial Arrangement:\n%v\n", freq)
+
+	totalStones := 0
 	for i := range *iterations {
-		// Evaluate chunks
-		chunkSize := (len(arang) + *cores) / *cores
-		log.Printf("Iteration: %d, chunkSize: %d\n", i+1, chunkSize)
-
-		var wg sync.WaitGroup
-		results := make([][]int, *cores)
-		reqCap := 0 // Required capacity to have for joined results
-		for coreIdx := range *cores {
-			start := coreIdx * chunkSize
-			end := start + chunkSize
-			if end > len(arang) {
-				end = len(arang)
-			}
-			if end < start {
-				// Already dealt with the array/ array smaller than core count
-				break
-			}
-
-			wg.Add(1)
-			go func(resultIndex int, arangement []int) {
-				defer wg.Done()
-				results[resultIndex] = AfterBlink(arangement)
-			}(coreIdx, arang[start:end])
-			reqCap += end - start
-		}
-		wg.Wait()
-		log.Printf("All chunks evaluated, number of stones: %d\n", reqCap)
-
-		// Join results together
-		newArang := make([]int, 0, reqCap)
-		for _, result := range results {
-			newArang = append(newArang, result...)
-		}
-		arang = newArang
-		log.Printf("Chunk results joined\n")
-
+		freq, totalStones = AfterBlink(freq)
 		if *verbose {
-			log.Printf("Arrangement after %d blinks:\n%v\n", i+1, arang)
+			log.Printf("Arrangement after %d iterations:\n%v\n", i, freq)
 		}
 	}
 
-	log.Printf("Total number of stones after %d iterations: %d\n", *iterations, len(arang))
+	log.Printf("Total number of stones after %d iterations: %d\n", *iterations, totalStones)
 }
